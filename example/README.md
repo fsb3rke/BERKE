ac7c672e2ab8fec20421bc8570ea5153a0ee6f29
Berke <69055194+Fsb3rKe@users.noreply.github.com>
2022-03-22 21:06:48 +0300

    Add files via upload

---
+++ example/README.md
@@ -0,0 +1,18 @@
+# BERKE
+
+## INTERPRETER COMMAND
+```bash
+python berke.py
+```
+
+## OUTPUT
+```
+> B
+> E
+> R
+> K
+> E
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/berke.py
@@ -0,0 +1,40 @@
+# tokens
+key = "BERK"
+u_keywords = [x.upper() for x in key] # + ASCII
+l_keywords = [x.lower() for x in key] # - ASCII
+
+file = open("hey.br", "r").read().split("\n")
+
+commentLines = [] # this is comment line indexes
+codeLines = [] # this is code line indexes
+for i in range(len(file)):
+    if file[i].startswith("BERKE"):
+        commentLines.append(i)
+    elif len(file[i]) == 0:
+        pass
+    else:
+        codeLines.append(i)
+
+codeLineCONTENT = []
+for i in codeLines:
+    codeLineCONTENT.append(file[i])
+
+#print(commentLines)
+#print(codeLines)
+
+interpetedLines = []
+
+for i in codeLineCONTENT:
+    i = i.replace("B", "+1")\
+            .replace("E", "+2")\
+            .replace("R", "+3")\
+            .replace("K", "+65")\
+            .replace("b", "-1")\
+            .replace("e", "-2")\
+            .replace("r", "-3")\
+            .replace("k", "-65")
+    
+    #print(eval(i))
+    asd = eval(i)
+    print(chr(asd))
+    #print(i)
